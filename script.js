de9efd73cc4e45dba467885686679e481885f64a
Julia-itcourse <66180630+Julia-itcourse@users.noreply.github.com>
2020-08-10 17:09:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-import gallery from './gallery-items.js';
+import gallery from "./gallery-items.js"
 
 //Создание и рендер разметки по массиву данных и предоставленному шаблону.
 
-const galleryList = document.querySelector('.js-gallery');
+const galleryList = document.querySelector(".js-gallery")
 
-let galleryItems = '';
+let galleryItems = ""
 gallery.forEach(
-  galleryItem =>
+  (galleryItem) =>
     (galleryItems += `
 <li class="gallery__item">
   <a
@@ -21,63 +21,65 @@ gallery.forEach(
     />
   </a>
 </li>
-`),
-);
+`)
+)
 
-galleryList.innerHTML = galleryItems;
+galleryList.innerHTML = galleryItems
 
-const lightboxElem = document.querySelector('.js-lightbox');
-const lightboxImage = document.querySelector('.lightbox__image');
-const closeIcon = document.querySelector('.lightbox__button');
-const overlay = document.querySelector('.lightbox__overlay');
+const lightboxElem = document.querySelector(".js-lightbox")
+const lightboxImage = document.querySelector(".lightbox__image")
+const closeIcon = document.querySelector(".lightbox__button")
+const overlay = document.querySelector(".lightbox__overlay")
 
 const openLightbox = function (event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') return;
+  event.preventDefault()
+  if (event.target.nodeName !== "IMG") return
   // console.log(event.target.getAttribute('data-source'));
-  lightboxElem.classList.add('is-open');
-  lightboxImage.src = event.target.getAttribute('data-source');
-};
+  lightboxElem.classList.add("is-open")
+  lightboxImage.src = event.target.getAttribute("data-source")
+}
 
 const closeLightbox = function () {
-  lightboxElem.classList.remove('is-open');
-  lightboxImage.src = '';
-};
+  lightboxElem.classList.remove("is-open")
+  lightboxImage.src = ""
+}
 
-galleryList.addEventListener('click', openLightbox);
-closeIcon.addEventListener('click', closeLightbox);
+galleryList.addEventListener("click", openLightbox)
+closeIcon.addEventListener("click", closeLightbox)
 
 //_______Additional tasks________________
 
 // Закрытие модального окна по нажатию клавиши ESC.
 
-window.addEventListener('keydown', function (event) {
-  if (event.key === 'Escape') {
-    lightboxElem.classList.remove('is-open');
+window.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    lightboxElem.classList.remove("is-open")
   }
-});
+})
 
 //? не работает Закрытие модального окна по клику на div.lightbox__overlay.
-
-overlay.addEventListener('click', closeLightbox);
+console.log(overlay)
+overlay.addEventListener("click", (event) => {
+  console.log(event.tagName)
+})
 
 // Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
 
 //* just some ideas on logic on gallery scroll
-const scrollRight = function (event) {
-  for (
-    let currentSlideIndex = 0;
-    currentSlideIndex < gallery.length;
-    currentSlideIndex++
-  ) {}
-};
-const scrollLeft = function (event) {
-  for (
-    let currentSlideIndex = 0;
-    currentSlideIndex < gallery.length;
-    currentSlideIndex--
-  ) {}
-};
+// const scrollRight = function (event) {
+//   for (
 
-window.addEventListener(keydown, scrollRight);
-window.addEventListener(keydown, scrollLeft);
+//     currentSlideIndex < gallery.length;
+//     currentSlideIndex++
+//   ) {}
+// };
+// const scrollLeft = function (event) {
+//   for (
+
+//     currentSlideIndex < gallery.length;
+//     currentSlideIndex--
+//   ) {}
+// };
+
+// window.addEventListener(keydown, scrollRight);
+// window.addEventListener(keydown, scrollLeft);
