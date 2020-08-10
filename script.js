f00d43e9882bd03ddf8597d3641e0ab918934962
Julia-itcourse <66180630+Julia-itcourse@users.noreply.github.com>
2020-08-10 21:42:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ import gallery from "./gallery-items.js"
 //Создание и рендер разметки по массиву данных и предоставленному шаблону.
 
 const galleryList = document.querySelector(".js-gallery")
+let imgIndex
+const createGalleryItems = function (items) {
+  let index = 0
 
-let galleryItems = ""
-gallery.forEach(
-  (galleryItem) =>
-    (galleryItems += `
-<li class="gallery__item">
+  return items.reduce((acc, galleryItem) => {
+    acc += `<li class="gallery__item">
   <a
     class="gallery__link"
     href="${galleryItem.original}"
@@ -18,14 +18,18 @@ gallery.forEach(
       src="${galleryItem.preview}"
       data-source="${galleryItem.original}"
       alt="${galleryItem.description}"
+      data-index="${index}"
 
     />
   </a>
 </li>
-`)
-)
+`
+    index += 1
+    return acc
+  }, "")
+}
 
-galleryList.innerHTML = galleryItems
+galleryList.innerHTML = createGalleryItems(gallery)
 
 const lightboxElem = document.querySelector(".js-lightbox")
 const lightboxImage = document.querySelector(".lightbox__image")
@@ -36,12 +40,18 @@ const openLightbox = function (event) {
   event.preventDefault()
   if (event.target.nodeName !== "IMG") return
   lightboxElem.classList.add("is-open")
-  lightboxImage.src = event.target.getAttribute("data-source")
+  lightboxImage.src = event.target.dataset.source
+
+  imgIndex = +event.target.dataset.index
+  window.addEventListener("keydown", scrollRight)
+  window.addEventListener("keydown", scrollLeft)
 }
 
 const closeLightbox = function () {
   lightboxElem.classList.remove("is-open")
   lightboxImage.src = ""
+  window.removeEventListener("keydown", scrollRight)
+  window.removeEventListener("keydown", scrollLeft)
 }
 
 galleryList.addEventListener("click", openLightbox)
@@ -68,25 +78,27 @@ content.addEventListener("click", (event) => {
 
 // Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
 
-// const scrollLeft = function () {
-//   if (event.key === "ArrowLeft") {
-//     imgIndex--
-//     if (imgIndex <= 0) {
-//       imgIndex = galery.length - 1
-//       lightboxImage.src = galery[index].src
-//     }
-//   }
-// }
+const scrollLeft = function () {
+  if (event.key === "ArrowLeft") {
+    imgIndex--
+    if (imgIndex < 0) {
+      imgIndex = gallery.length - 1
+      lightboxImage.src = gallery[imgIndex].original
+    } else {
+      lightboxImage.src = gallery[imgIndex].original
+    }
+  }
+}
 
-// const scrollRight = function () {
-//   if (event.key === "ArrowRight") {
-//     imgIndex--
-//     if (imgIndex > 0) {
-//       imgIndex = galery.length + 1
-//       lightboxImage.src = galery[index].src
-//     }
-//   }
-// }
-
-// window.addEventListener(keydown, scrollRight)
-// window.addEventListener(keydown, scrollLeft)
+const scrollRight = function () {
+  if (event.key === "ArrowRight") {
+    imgIndex++
+    if (imgIndex > gallery.length - 1) {
+      imgIndex = 0
+      lightboxImage.src = gallery[imgIndex].original
+    } else {
+      lightboxImage.src = gallery[imgIndex].original
+    }
+  }
+}
+console.log(gallery[0])
